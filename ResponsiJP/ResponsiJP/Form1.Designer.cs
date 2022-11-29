62fb44174a3fa280c119083439dc7943fb7f4fe3
Raisa Salsabil Yusriyya <raisa.salsabil@mail.ugm.ac.id>
2022-11-29 08:33:41 +0700

    feat: added insert and load

--- ResponsiJP/ResponsiJP/Form1.Designer.cs
+++ ResponsiJP/ResponsiJP/Form1.Designer.cs
@@ -36,15 +36,16 @@
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvData = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -89,6 +90,7 @@
             this.btnInsert.TabIndex = 4;
             this.btnInsert.Text = "Insert";
             this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
             // 
             // btnEdit
             // 
@@ -115,13 +117,49 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(431, 82);
+            this.groupBox1.Location = new System.Drawing.Point(428, 21);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(147, 149);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "ID Departemen:";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "FIN: Finance";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "PM: Product M";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "DEV: Developer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ENG: Engineer";
             // 
             // label2
             // 
@@ -133,50 +171,14 @@
             this.label2.Text = "HR: HR";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label3
+            // dgvData
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 53);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 15);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "ENG: Engineer";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 73);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 15);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "DEV: Developer";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 94);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(87, 15);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "PM: Product M";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(17, 114);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(72, 15);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "FIN: Finance";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(40, 253);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(535, 289);
-            this.dataGridView1.TabIndex = 8;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(40, 253);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowTemplate.Height = 25;
+            this.dgvData.Size = new System.Drawing.Size(535, 289);
+            this.dgvData.TabIndex = 8;
             // 
             // pictureBox1
             // 
@@ -186,13 +188,24 @@
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(500, 208);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 10;
+            this.btnLoad.Text = "Load Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 587);
+            this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvData);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
@@ -203,9 +216,10 @@
             this.Controls.Add(this.lblNama);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -227,7 +241,8 @@
         private Label label5;
         private Label label4;
         private Label label3;
-        private DataGridView dataGridView1;
+        private DataGridView dgvData;
         private PictureBox pictureBox1;
+        private Button btnLoad;
     }
 }
--- ResponsiJP/ResponsiJP/Form1.cs
+++ ResponsiJP/ResponsiJP/Form1.cs
@@ -1,3 +1,6 @@
+using Npgsql;
+using System.Data;
+
 namespace ResponsiJP
 {
     public partial class Form1 : Form
@@ -5,6 +8,17 @@ namespace ResponsiJP
         public Form1()
         {
             InitializeComponent();
+        }
+        private NpgsqlConnection conn;
+        string connstring = "Host=localhost;Port=2022;Username=raisasalsabil;Password=Raisa10112001baru;Database=resp_raisa";
+        public DataTable dt;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private DataGridViewRow r;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
@@ -15,6 +29,50 @@ namespace ResponsiJP
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"select * form insert_func(:_nama, :_id_dep)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_nama", tbNama.Text);
+                cmd.Parameters.AddWithValue("_id_dep", tbDep.Text);
+                if((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Berhasil Memasukkan Data!");
+                    conn.Close();
+                    btnLoad.PerformClick();
+                    tbNama.Text = tbDep.Text = null;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message + " FAIL!");
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                dgvData.DataSource = null;
+                sql = "select * from select_func()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                dgvData.DataSource = dt;
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message + " FAIL!");
+            }
         }
     }
 }
